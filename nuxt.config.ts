b3070328d8c9b1cfbb7c64a8e7fa1b1f91a73205
Mizu <psdk55052004@gmail.com>
2023-12-28 16:09:09 +0800

    新增 設定特定資料夾才套用 Auto Import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  // 設定特定的資料夾才可 Auto Import, 其餘資料夾需手動 import
+  // "components": {
+  //   "dirs": [
+  //     {
+  //       path: '~/components/<資料夾名稱>',
+  //       global: true
+  //     }
+  //   ]
+  // }
 })
