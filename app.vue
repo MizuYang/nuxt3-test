f13f4d0b33d296b8f36bf6b5e1f21761294ca8c4
Mizu <psdk55052004@gmail.com>
2023-12-28 14:20:52 +0800

    新增 layouts 資料夾, 進入點由 app.vue 轉為 => layouts/default.vue

--- app.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <header>header區塊</header>
-    <NuxtPage />
-    <footer>footer區塊</footer>
-  </div>
-</template>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div>
+    <header>header 區塊</header>
+    <slot />
+    <footer>footer 區塊</footer>
+  </div>
+</template>
+
+<style scoped></style>
