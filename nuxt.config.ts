89eda4c26a648eebb936b1ffb138f8f2b2983475
Mizu <psdk55052004@gmail.com>
2024-01-03 11:45:55 +0800

    新增 meta tag 優化搜尋引擎

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
+  // [全域] 優化 SEO (meta tag)
+  "app": {
+    "head": {
+      "viewport": "width=500, initial-scale=1",
+      "title": "Nuxt3 練習",
+      "meta": [
+        { "name": "description", "content": "這是 Mizu 的 Nuxt3 練習網頁" },
+        { "property": "title", "content": "Nuxt3 練習 desu~" },
+        { "property": "og:title", "content": "Nuxt3 練習 desu~" },
+        { "property": "og:url", "content": "http://localhost:3000/" },
+        { "property": "og:description", "content": "這是 Mizu 的 Nuxt3 練習網頁" },
+      ]
+    }
+  },
+
   // 設定特定的資料夾才可 Auto Import, 其餘資料夾需手動 import
   // "components": {
   //   "dirs": [
