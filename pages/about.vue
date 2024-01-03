d1601b2097d419d65324984b165ceb2d7e08843a
Mizu <psdk55052004@gmail.com>
2024-01-03 11:59:44 +0800

    新增 useHead 來為 about 頁面設定 meta tag

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,21 @@
 <script setup>
 definePageMeta({
- middleware: (to, from) => {
-   console.log("匿名 middleware about 頁面 =>", { to, from });
- },
+  middleware: (to, from) => {
+    console.log("匿名 middleware about 頁面 =>", { to, from });
+  },
+});
+
+// 單一設定 meta tag
+useHead({
+  // useHead 的所有屬性,可參考：https://nuxt.com/docs/api/composables/use-head#usehead
+  title: "這是關於 Mizu 的頁面",
+  meta: [
+    { property: "og:title", content: "這是關於 Mizu 的頁面" },
+    { property: "og:url", content: "http://localhost:3000/about" },
+    { property: "og:image", content: "http://localhost:3000/share.jpg" },
+    { name: "description", content: "關於 Mizu 頁面" },
+    { property: "og:description", content: "關於 Mizu 頁面" },
+  ],
 });
 </script>
 
@@ -13,7 +26,7 @@ definePageMeta({
     <UserComponent />
   </div>
 
-  <NuxtLink to="/" style="font-size:25px;">回到首頁</NuxtLink>
+  <NuxtLink to="/" style="font-size: 25px">回到首頁</NuxtLink>
 </template>
 
 <style scoped></style>
