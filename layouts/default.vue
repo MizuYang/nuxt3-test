cadd65f6d4d4187611df416a4e6eeeece58b99b5
Mizu <psdk55052004@gmail.com>
2024-01-03 15:59:58 +0800

    新增 在 layout 新增 頁面切換按鈕

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,30 @@
-<script setup></script>
+<script setup>
+const pages = {
+  home: {
+    path: '/',
+    name: '首頁'
+  },
+  about: {
+    path: 'about',
+    name: 'about'
+  },
+  user: {
+    path: 'user',
+    name: 'user'
+  },
+}
+</script>
 
 <template>
   <div>
-    <header>header 區塊</header>
+    <header>
+      header 區塊
+      <ul>
+        <li v-for="page in pages" :key="page.name">
+          <NuxtLink :to="page.path">{{ page.name }}</NuxtLink>
+        </li>
+      </ul>
+    </header>
     <slot />
     <footer>footer 區塊</footer>
   </div>
