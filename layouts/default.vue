492808fb1c289ef0faffb2df0a566bb9a504b53c
Mizu <psdk55052004@gmail.com>
2024-01-09 15:10:26 +0800

    新增 nuxtApp.hook 監聽頁面切換、切換結束, 客製化 Loading

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,6 +25,18 @@ const pages = {
     name: 'courses (接課程API練習嵌套路由)'
   },
 }
+
+//! loading 方法二: 自訂 loading 元件
+// const nuxtApp = useNuxtApp();
+// const isLoading = ref(false);
+// nuxtApp.hook("page:start", () => {
+//   console.log('page:start')
+//   isLoading.value = true;
+// });
+// nuxtApp.hook("page:finish", () => {
+//   console.log('page:finish')
+//   isLoading.value = false;
+// });
 </script>
 
 <template>
@@ -47,14 +59,30 @@ const pages = {
     <slot />
     <footer>footer 區塊</footer>
 
-    <!-- loading 元件 -->
+    <!--//! loading 方法一: NuxtLoadingIndicator -->
     <NuxtLoadingIndicator color="#f00" 
                           :height="10" 
                           :throttle="0">
       <p style="text-align:center;">Loading...</p>
     </NuxtLoadingIndicator>
 
+    <!--//! loading 方法二: 自訂 loading 元件 -->
+    <!-- <div v-show="isLoading" class="loading">
+      Loading
+    </div> -->
+
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  font-size: 50px;
+  color: #fff;
+  background-color: #00000052;
+}
+</style>
