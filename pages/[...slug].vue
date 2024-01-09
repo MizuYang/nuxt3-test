0593e7a961f883c797f29962136271f3728c6226
Mizu <psdk55052004@gmail.com>
2024-01-09 15:55:51 +0800

    新增 404頁面

---
+++ pages/[...slug].vue
@@ -0,0 +1,13 @@
+<script setup>
+const route = useRoute();
+const event = useRequestEvent();
+setResponseStatus(event, 404);
+</script>
+
+<template>
+  <h1>404 Not Found</h1>
+  <p>錯誤網址:{{ route.params.slug }}</p>
+  <NuxtLink to='/'>
+    <p style="font-size:25px;">返回首頁</p>
+  </NuxtLink>
+</template>
