0b9baf9f63644c2873e6461ac29bffe39823ec56
Mizu <psdk55052004@gmail.com>
2024-01-09 10:18:00 +0800

    新增 取得單一課程 API

--- pages/courses/[coursesId].vue
+++ pages/courses/[coursesId].vue
@@ -1,8 +1,16 @@
 <script setup>
 const route = useRoute()
-console.log(route)
 const { coursesId } = route.params
+const data = ref({})
 
+await getCourse()
+
+async function getCourse() {
+  const api = `https://vue-lessons-api.vercel.app/courses/${coursesId}`
+  data.value = await useAsyncData('test-course-2', async () => {
+    return await $fetch(api)
+  })
+}
 </script>
 
 <template>
@@ -12,6 +20,8 @@ const { coursesId } = route.params
   </NuxtLink>
 
   {{ coursesId }}
+
+  {{ data }}
 </template>
 
 <style lang='scss' scope></style>
