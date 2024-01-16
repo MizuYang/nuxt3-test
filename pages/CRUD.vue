353fb2917cff1e5e0e9b5c897f466ea6dfcfcf77
Mizu <psdk55052004@gmail.com>
2024-01-16 09:44:25 +0800

    新增 CRUD: 新增用戶按鈕、資料變數

--- pages/CRUD.vue
+++ pages/CRUD.vue
@@ -1,10 +1,41 @@
 <script setup>
 // get api
 const { data: peopleData } = await useFetch("/api/people");
+
+// post api
+const form = ref({
+  name: "",
+  email: "",
+  age: "",
+})
+function addPeople() {
+
+}
 </script>
 
 <template>
-  <!-- CRUD: get api -->
+  <!-- CRUD: post -->
+  <form>
+    <div>
+      <input type="text" 
+             placeholder="名字"
+             v-model.trim="form.name"
+             style="text-align:center;width:80px;">
+      <input type="text" 
+             placeholder="信箱"
+             v-model.trim="form.email"
+             style="text-align:center;width:130px;">
+      <input type="number" 
+             placeholder="年齡"
+             v-model.number.trim="form.age"
+             style="text-align:center;width:80px;">
+      <button type="button" @click="addPeople">
+        新增用戶
+      </button>
+    </div>
+  </form>
+
+  <!-- CRUD: get -->
   <ul>
     <template v-for="people in peopleData" :key="people._id">
       <li>
