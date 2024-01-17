bdcec65478944bafe66dc8fc356c3dd1d688fa71
Mizu <psdk55052004@gmail.com>
2024-01-17 16:14:18 +0800

    新增 登入機制開發: 登入驗證

--- composables/useLogin.js
+++ composables/useLogin.js
@@ -1,4 +1,12 @@
+
 export function useLogin() {
+  // data
+  const userInfo = ref({})
+  const cookie = useCookie('nuxt-mizu-login-token')
+
+  onMounted(() => {
+    checkLogin()
+  })
 
   async function userLogin() {
     const api = 'https://vue-lessons-api.vercel.app/auth/login'
@@ -10,14 +18,36 @@ export function useLogin() {
         password: '7654321',
       }
     })
-    console.log(res)
+    userInfo.value = res.data
+    
     // 將 toekn 存到 cookie
-    const cookie = useCookie('nuxt-mizu-login-token')
     cookie.value = res.data.token // 設定值
     console.log(cookie.value) // 讀取值
   }
+  async function checkLogin() {
+    const token = cookie
+    
+    // 有登入 token 才驗證登入狀態
+    if(!token.value) return
+    
+    try {
+      const res = await $fetch('https://vue-lessons-api.vercel.app/testToken', {
+        method: 'POST',
+        headers: {
+          Authorization: token.value
+        }
+      })
+      console.log(res)
+      // 登入驗證成功: 可在此處取得用戶資料
+      userInfo.value = { name: "成智遠" }
+    } catch (err) {
+      console.error(err)
+      // 登入驗證失敗: 可將用戶返回登入頁面
+    }
+  }
 
   return {
-    userLogin
+    userLogin,
+    userInfo
   }
 }
--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,19 @@
 <script setup>
-const { userLogin } = useLogin()
+const { userInfo, userLogin } = useLogin()
+
 </script>
 
 <template>
-  <button type='button' @click="userLogin" style="font-size:50px;">
-    登入
-  </button>
+  <template v-if="userInfo.name">
+    <span style="font-size:35px;color:green;">
+      {{ userInfo.name }} 登入成功!
+    </span>
+  </template>
+  <template v-else>
+    <button type='button' @click="userLogin" style="font-size:50px;">
+      登入
+    </button>
+  </template>
 </template>
 
 <style lang='scss' scope></style>
